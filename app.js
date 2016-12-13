fcc3f18461c53196f40821315ff76796820caa12
bernadettekelly <bernadettemkelly54@gmail.com>
2016-12-12 21:09:44 -0500

    second commit

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-var Youtube-base-URL = https:"//www.googleapis.com/youtube/v3/search";
+var Youtube_base_URL = "https://www.googleapis.com/youtube/v3/search";
 
 function getDataFromApi (searchTerm, callback) {
 	var query = {
-		part: 'snippet'
-		r: 'json'
-		key: 'AIzaSyAiZvfIQ2sqypbAn7n6tN5Cgn1wLeXmwGA'
-		q: 'string'
+		part: 'snippet',
+		r: 'json',
+		key: 'AIzaSyAiZvfIQ2sqypbAn7n6tN5Cgn1wLeXmwGA',
+		q: searchTerm
 	}
-	$.getJSON(Youtube-base-URL, query, callback);
+	$.getJSON(Youtube_base_URL, query, callback);
 }
 
 function showSearchResults(data) {
 	var result = '';
-	if (data.Search) {
-		data.Search.forEach(function(item) {
-			result += '<p>' + item.Title + '</p>';
+	if (data.items) {
+		data.items.forEach(function(item) {
+			result += '<p>' + item.id.videoId + '</p>';
 		});
 	}
 	else {
 		result += '<p>No results found</p>';
 	}
-	$('search-results').html(result);
+	$('.search-results').html(result);
 }
 
 function submitForm() {
-	$('search-form').submit(function(event) {
-		event.PreventDefault();
+	$('.search-form').submit(function(event) {
+		event.preventDefault();
 		var query = $(this).find('.query-input').val();
 		getDataFromApi(query, showSearchResults);
 	});
 }
 
-(function(){submitForm();});
+// (function(){submitForm();});
+$(document).ready(function(){submitForm()});
